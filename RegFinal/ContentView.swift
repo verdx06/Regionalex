edc75345839afe09f43c2a9621235bfc61e53962
verdx <berkut589243@gmail.com>
2025-01-21 14:13:14 +0700

    Initial Commit

---
+++ RegFinal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RegFinal
+//
+//  Created by Виталий Багаутдинов on 21.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RegFinal/RegFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegFinalApp.swift
+//  RegFinal
+//
+//  Created by Виталий Багаутдинов on 21.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct RegFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
