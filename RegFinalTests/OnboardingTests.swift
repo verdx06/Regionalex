d406d21b081815fafabc9e04f5d9174236209874
verdx <berkut589243@gmail.com>
2025-01-24 20:51:22 +0700

    Tests

---
+++ RegFinalTests/OnboardingTests.swift
@@ -0,0 +1,23 @@
+//
+//  OnboardingTests.swift
+//  RegFinalTests
+//
+//  Created by Виталий Багаутдинов on 22.01.2025.
+//
+
+import XCTest
+@testable import RegFinal
+
+final class OnboardingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+    
+}
